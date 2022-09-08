908829e3ffaa82471a1f73d0104bebd1012e48b5
doYourCode <caio.lamas@ifnmg.edu.br>
2022-09-07 22:10:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # TODO abstrair a função main para um objeto Game, responsável pela execução do código, o objetivo é legibilidade
-from codigo_fonte.Jogo import *
+#from codigo_fonte.Jogo import *
+from codigo_fonte.testes._05_pontos import *
 
 
 if __name__ == '__main__':
-    jogo = Jogo()
-    jogo.executar()
+    game_run()
+    #jogo = Jogo()
+    #jogo.executar()
